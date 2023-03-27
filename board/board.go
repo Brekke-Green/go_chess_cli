142f64475e933a4425f0f2f67f32d78aa308dfa5
Brekke Green <brekke.andrew.green@gmail.com>
2023-03-27 11:47:30 -0400

    Build basic board const values

---
+++ board/board.go
@@ -0,0 +1,17 @@
+package board
+
+import (
+    "strings"
+)
+
+const (
+    Rows = 8
+    Cols = 8
+)
+
+const (
+    StartRow = 0
+    StartCol = 0
+    EndRow = Rows - 1
+    EndCol = Cols - 1
+)
